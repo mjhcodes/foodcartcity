f9403c999adbd46236d20af82c2a06bfac507038
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-07 01:04:21 -0700

    enabled dynamic side nav popout on click

--- fcc_django/static/js/vue.js
+++ fcc_django/static/js/vue.js
@@ -1,31 +1,82 @@
+///////////// VUE STORES //////////////
 
+const store = new Vuex.Store({
+  state: {
+    cartObject: ""
+  },
+  mutations: {
+    passCart (state, currentCart) {
+      state.cartObject = currentCart
+    }
+  },
+});
 
 ///////////// VUE COMPONENTS //////////////
 
+Vue.component("navbar", {
+  template: `
+    <div>
+      <nav>
+        <div class="nav-wrapper light-green lighten-5">
+          <a href="#!" class="brand-logo"><img src="../static/img/fcclogo.png" style="height: 50px"></a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">SIGN IN</a></li>
+            <li><a href="#">GET STARTED</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul class="sidenav" id="mobile-demo">
+        <li><a href="#">SIGN IN</a></li>
+        <li><a href="#">GET STARTED</a></li>
+      </ul>
+    </div>
+  `
+})
+
 Vue.component("card", {
-	props: ["image", "name", "address", "phone", "website"],
-	data: function() {
-		return {
-			tel: "tel:",
-		}
-	},
+  props: ["cart"],
+  methods: {
+    cartSideNav(currentCart) {
+      store.commit("passCart", currentCart)
+    }
+  },
 	template: `
-		<div class="card medium">
-			<div class="card-image center-align">
-				<img :src="image" alt="food cart city logo">
-				<span class="card-title center-align">{{ name }}</span>
-			</div>
-			<div class="card-content center-align valign-wrapper">
-				<p>{{ address }}</p>
-			</div>
-			<div class="card-action center-align">
-				<a :href="tel + phone"><i class="material-icons">local_phone</i></a>
-				<a :href="website"><i class="material-icons">desktop_mac</i></a>
+		<div class="card hoverable">
+			<div class="card-image waves-effect waves-block waves-light">
+        <a href="#" data-target="slide-out" class="sidenav-trigger" @click="cartSideNav(cart)"><img :src="cart.image" alt="food cart city logo"></a>
 			</div>
 		</div>
 	`
 })
 
+Vue.component("slideout", {
+	data: function() {
+		return {
+      tel: "tel:",
+      googleMaps: "https://www.google.com/maps/place/",
+		}
+  },
+  computed: {
+    cart() {
+      return store.state.cartObject
+    }
+  },
+  template: `
+    <div id="slide-out" class="sidenav">
+      <div class="user-view" style="height: 285px">
+        <div class="background">
+          <img :src="cart.image" style="width: 100%">
+        </div>
+      </div>
+      <h4>{{ cart.name }}</h4>
+      <div class="divider"></div>
+      <p><a :href="googleMaps + cart.address" target="_blank">{{ cart.address }}</a></p>
+      <div class="divider"></div>
+    </div>
+  `
+})
 
 
 ///////////// VUE ROOT //////////////
@@ -40,9 +91,9 @@ new Vue({
 			searchValue: null,
 			cuisineFilter: null, // not yet a feature
 			page: 1,
-			perPage: 8,
+      perPage: 8,
 			carts: [],
-			cuisines: [],
+      cuisines: [],
 		}
 	},
   methods: {
@@ -76,20 +127,22 @@ new Vue({
         console.log(error);
       })
     },
-    prevPage: function () {
-      if (this.page > 1) {
-        this.page--;
-        this.listCarts();
-      }
-    },
-    nextPage: function () {
-      if (this.page <= (this.carts.length / this.perPage) + 1) 
-        this.page++;
-        this.listCarts();
-    },
+    // prevPage: function () {
+    //   if (this.page > 1) {
+    //     this.page--;
+    //     this.listCarts();
+    //   }
+    // },
+    // nextPage: function () {
+    //   if (this.page <= (this.carts.length / this.perPage) + 1) 
+    //     this.page++;
+    //     this.listCarts();
+    // },
   },
   mounted: function () {
     this.listCarts();
     this.listCuisines();
   }
 })
+
+M.AutoInit();
