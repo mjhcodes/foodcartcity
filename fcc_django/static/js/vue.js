c3aef5c1e87ead57cc09fcf1791a9726d310effe
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-10 00:07:16 -0700

    fixed pagination bug and styled sidenav

--- fcc_django/static/js/vue.js
+++ fcc_django/static/js/vue.js
@@ -24,22 +24,22 @@ Vue.component("navbar", {
     <div>
       <nav>
         <div class="nav-wrapper white">
-          <a href="#!" class="brand-logo"><img src="../static/img/fcclogo.png" style="height: 50px"></a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons green-text text-darken-4">menu</i></a>
+          <a href="#!" class="brand-logo"><img src="../static/img/fcclogo.png" style="height: 50px;"></a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger" style="margin-left: 8.5%;"><i class="material-icons green-text text-darken-4">menu</i></a>
           <ul class="right hide-on-med-and-down">
             <li>
               <div class="search-wrapper focused">
                 <input class="black-text" id="search" placeholder="Search" v-bind:value="value" v-on:input="updateValue($event.target.value)" />
               </div>
             </li>
-            <li style="margin-left: 25px;"><a href="#" class="green-text text-darken-4">SIGN IN</a></li>
-            <li><a href="#" class="green darken-4 white-text">GET STARTED</a></li>
+            <li class="center-align" style="width: 140px; margin-left: 35px;"><a href="#" class="green-text text-darken-4" style="padding-left: 25px; padding-right: 25px;">SIGN IN</a></li>
+            <li style="width: 140px"><a href="#" class="green darken-4 white-text" style="padding-left: 20px; padding-right: 20px;">GET STARTED</a></li>
           </ul>
         </div>
       </nav>
 
       <ul class="sidenav" id="mobile-demo">
-        <a href="#!" class="brand-logo"><img src="../static/img/fcclogo.png" style="width: 90%; margin-top: 20px; margin-bottom: 20px;"></a>
+        <a href="#!" class="brand-logo"><img src="../static/img/fcclogo.png" style="width: 90%; margin-top: 25px; margin-bottom: 25px;"></a>
         <li>
           <div class="search-wrapper focused center-align">
             <input style="width: 80%;" id="search" placeholder="Search" v-bind:value="value" v-on:input="updateValue($event.target.value)" />
@@ -90,27 +90,31 @@ Vue.component("slideout", {
   },
   template: `
     <div id="slide-out" class="sidenav">
-      <div class="user-view" style="margin: 0; height: 285px">
+      <div class="user-view" style="margin: 0; height: 300px">
         <div class="background">
           <img :src="cart.image" style="width: 100%">
         </div>
       </div>
-      <div style="height: 75px" class="valign-wrapper">
-        <h4 style="margin: 0; width: 100%" class="center-align">{{ cart.name }}</h4>
+      <div style="height: 22.5vh; padding-right: 10%; padding-left: 10%;" class="valign-wrapper">
+        <h3 style="margin: 0; width: 100%" class="center-align">{{ cart.name }}</h3>
       </div>
       <div style="margin: 0" class="divider"></div>
-      <div style="height: 100px" class="valign-wrapper">
-        <a :href="googleMaps + cart.address" target="_blank" style="width: 100%">{{ cart.address }}</a>
+      <div style="height: 10vh; padding-right: 10%; padding-left: 10%;" class="valign-wrapper">
+        <p style="margin: 0; width: 100%;" class="center-align" v-for="cuisine in cart.cuisines">{{ cuisine.name }}</p>
       </div>
       <div style="margin: 0" class="divider"></div>
-      <div style="display: flex; justify-content: space-evenly; height: 50px;" class="valign-wrapper">
-        <a :href="tel + cart.phone" v-if="cart.phone"><i class="fas fa-phone-alt fa-lg"></i></a>
-        <a :href="cart.website" v-if="cart.website" target="_blank"><i class="fas fa-desktop fa-lg"></i></a>
-        <a :href="mailto + cart.email" v-if="cart.email"><i class="fas fa-paper-plane fa-lg"></i></a>
-        <a :href="cart.facebook" v-if="cart.facebook" target="_blank"><i class="fab fa-facebook fa-lg"></i></a>
-        <a :href="cart.instagram" v-if="cart.instagram" target="_blank"><i class="fab fa-instagram fa-lg"></i></a>
-        <a :href="cart.twitter" v-if="cart.twitter" target="_blank"><i class="fab fa-twitter fa-lg"></i></a>
-        <a :href="cart.yelp" v-if="cart.yelp" target="_blank"><i class="fab fa-yelp fa-lg"></i></a>
+      <div style="height: 17.5vh; padding-right: 10%; padding-left: 10%;" class="valign-wrapper grey lighten-5">
+        <a :href="googleMaps + cart.address" target="_blank" class="black-text" style="width: 100%">{{ cart.address }}</a>
+      </div>
+      <div style="margin: 0" class="divider"></div>
+      <div style="display: flex; justify-content: space-evenly; height: 10vh;" class="valign-wrapper">
+        <a :href="tel + cart.phone" v-if="cart.phone"><i class="green-text text-darken-3 fas fa-phone-alt fa-lg"></i></a>
+        <a :href="cart.website" v-if="cart.website" target="_blank"><i class="green-text text-darken-3 fas fa-desktop fa-lg"></i></a>
+        <a :href="mailto + cart.email" v-if="cart.email"><i class="green-text text-darken-3 fas fa-paper-plane fa-lg"></i></a>
+        <a :href="cart.facebook" v-if="cart.facebook" target="_blank"><i class="green-text text-darken-3 fab fa-facebook fa-lg"></i></a>
+        <a :href="cart.instagram" v-if="cart.instagram" target="_blank"><i class="green-text text-darken-3 fab fa-instagram fa-lg"></i></a>
+        <a :href="cart.twitter" v-if="cart.twitter" target="_blank"><i class="green-text text-darken-3 fab fa-twitter fa-lg"></i></a>
+        <a :href="cart.yelp" v-if="cart.yelp" target="_blank"><i class="green-text  text-darken-3 fab fa-yelp fa-lg"></i></a>
       </div>
       <div style="margin: 0" class="divider"></div>
     </div>
@@ -130,7 +134,7 @@ new Vue({
 			searchValue: null,
 			cuisineFilter: null, // not yet a feature
 			page: 1,
-      perPage: 8,
+      perPage: 12,
       pages: [],
 			carts: [],
       cuisines: [],
@@ -169,6 +173,7 @@ new Vue({
     },
     setPages () {
       let numberOfPages = Math.ceil(this.carts.length / this.perPage);
+      this.pages = [];
       for (let index = 1; index <= numberOfPages; index++) {
         this.pages.push(index);
       }
@@ -179,7 +184,17 @@ new Vue({
       let from = (page * perPage) - perPage;
       let to = (page * perPage);
       return carts.slice(from, to);
-    }
+    },
+    prevPage () {
+      if (this.page != 1) {
+        this.page--
+      }
+    },
+    nextPage () {
+      if (this.page < Math.ceil(this.carts.length / this.perPage)) {
+        this.page++
+      }
+    },
   },
   mounted: function () {
     this.listCarts();
